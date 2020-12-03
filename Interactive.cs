697ed46bbb1a54634add1d5c70564ba37e7554b7
AugusteCousin <34842568+AugusteCousin@users.noreply.github.com>
2020-12-03 22:45:55 +0100

    Add files via upload

---
+++ Interactive.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+namespace WasaaMP {
+public class Interactive : MonoBehaviourPun {
+    // Start is called before the first frame update
+
+    MonoBehaviourPun support = null ;
+    public Rigidbody rb;
+
+    void Start () {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate () {
+        rb.velocity=new Vector3(0, 0, 0);
+        
+    }
+    void OnTriggerEnter (Collider other) {
+        print (name + " : OnCollisionEnter") ;
+		var hit = other.gameObject ;
+		var cursor = hit.GetComponent<CursorTool> () ;
+		if (cursor != null) {
+			Renderer renderer = GetComponentInChildren <Renderer> () ;
+		    renderer.material.color = Color.blue ;
+		}
+	}
+    
+    void OnTriggerExit (Collider other) {
+        print (name + " : OnCollisionExit") ;
+		var hit = other.gameObject ;
+		var cursor = hit.GetComponent<CursorTool> () ;
+		if (cursor != null) {
+			Renderer renderer = GetComponentInChildren <Renderer> () ;
+		    renderer.material.color = Color.white ;
+		}
+	}
+
+    public void SetSupport (MonoBehaviourPun support) {
+        this.support = support ;
+        if (support != null) {
+            transform.SetParent (support.transform) ;
+        } else {
+            transform.SetParent (null) ;
+        }
+    }
+
+    public void RemoveSupport () {
+        transform.SetParent (null) ;
+        support = null ;
+    }
+
+    public MonoBehaviourPun GetSupport () {
+        return support ;
+    }
+}
+
+}
---
+++ ManagerStick.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerStick : MonoBehaviour
+{
+    private GameObject handle1, handle2;
+    private GameObject[] balloons;
+
+    private Vector3 translateZ=new Vector3(0f, 0f, 0.6f);
+
+    private bool displayMessage = false;
+    private float displayTime = 3;
+    private string message="what";
+    private GameObject poppedMessage;
+    private GameObject allPoppedMessage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        handle1=GameObject.Find("handle1").gameObject;
+        handle2=GameObject.Find("handle2").gameObject;
+        poppedMessage=GameObject.Find("popped").gameObject;
+        poppedMessage.SetActive(false);
+        
+        allPoppedMessage=GameObject.Find("allPopped").gameObject;
+        allPoppedMessage.SetActive(false);
+        //this.GetComponent<Rigidbody>().useGravity=false;
+        balloons=GameObject.FindGameObjectsWithTag("Balloon");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.localPosition=new Vector3((handle1.transform.position.x+handle2.transform.position.x)/2, (handle1.transform.position.y+handle2.transform.position.y)/2, (handle1.transform.position.z+handle2.transform.position.z)/2);
+        this.transform.LookAt(handle1.transform.position);
+
+        if (displayMessage){
+            displayTime-=Time.deltaTime;
+            if (displayTime<=0.0){
+                displayMessage=false;
+                poppedMessage.SetActive(false);
+            }
+
+        }
+        balloons=GameObject.FindGameObjectsWithTag("Balloon");
+        if (balloons.Length<1){
+            allPoppedMessage.SetActive(true);
+        }
+        
+        
+        //if handle1 has been caught by user
+        // if (!this.transform.Find("handle1")){
+        //     this.transform.position=handle1.transform.position;
+        //     this.transform.Translate(new Vector3(0f, 0f, -3f));
+        //     this.transform.rotation=handle1.transform.rotation;
+        // }
+        // if (!this.transform.Find("handle2")){
+        //     this.transform.position=handle2.transform.position;
+        //     this.transform.Translate(new Vector3(0f, 0f, 3f));
+        //     this.transform.rotation=handle2.transform.rotation;
+        // }
+        
+    }
+
+    void OnTriggerEnter (Collider other) {
+
+        if (other.gameObject.tag == "Balloon")
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            print("STONKS");
+            Destroy(other.gameObject);
+            displayMessage=true;
+            displayTime=3;
+            if (balloons.Length>1){
+                poppedMessage.SetActive(true);
+            }
+        }
+	}
+    
+}
